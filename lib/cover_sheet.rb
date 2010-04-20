ee3cbe0c92ec175a3a97311c1b08855f2895e473
Victor Costan <costan@gmail.com>
2010-04-20 18:36:13 -0400

    Better tables.

--- lib/cover_sheet.rb
+++ lib/cover_sheet.rb
@@ -1,7 +1,4 @@
 # compiles nice cover sheets
-require 'pdf/writer'
-require 'pdf/simpletable'
-
 module CoverSheet
   # crap included because we're doing view stuff in the controller :(
   include ActionView::Helpers::DateHelper
@@ -15,24 +12,24 @@ module CoverSheet
         index_by { |s| s.deliverable.id }
   
     # letter: 612pts x 792pts
-    pdf = PDF::Writer.new :paper => 'LETTER', :orientation => :portrait
+    pdf = Prawn::Document.new :page_size => 'LETTER', :page_layout => :portrait
+    
     
     # course footer
     course = Course.main
-    pdf.select_font "Helvetica"
+    pdf.font "Helvetica"
     pdf.y = 40 + 18 * 4
     pdf.font_size = 14
-    pdf.text "#{course.number} - #{course.title}", :justification => :center
-    pdf.text "Massachusetts Institute of Technology", :justification => :center
+    pdf.text "#{course.number} - #{course.title}", :align => :center
+    pdf.text "Massachusetts Institute of Technology", :align => :center
     pdf.text "Department of Electrical Engineering and Computer Science",
-             :justification => :center
-    pdf.move_pointer 14
-    pdf.text "Designed by Victor Costan", :justification => :center,
-                                          :font_size => 8
+             :align => :center
+    pdf.y -= 14
+    pdf.text "Designed by Victor Costan", :align => :center, :size => 8
 
     # Recitation Section
     pdf.y = 792 - 36
-    pdf.select_font "Times-Roman"
+    pdf.font "Times-Roman"
     user = target.respond_to?(:users) ? target.users.first : target
     if Course.main.has_recitations?
       if user.profile and user.profile.recitation_section
@@ -44,146 +41,100 @@ module CoverSheet
     else
       section_title = ""
     end
-    pdf.text section_title, :justification => :left, :font_size => 24
+    pdf.text section_title, :align => :left, :size => 24
     
     # MIT name and real name
-    pdf.select_font "Courier"
+    pdf.font "Courier"
     target_title = target.respond_to?(:email) ?
          target.email.split('@', 2).first : target.name
-    pdf.text target_title, :justification => :left, :font_size => 36
-    pdf.select_font "Times-Roman"
+    pdf.text target_title, :align => :left, :size => 36
+    pdf.font "Times-Roman"
     if target.respond_to?(:users)
       target.users.each do |user|
         pdf.text((user.profile.nil? ? user.name :
                   user.profile.real_name),
-                 :justification => :left, :font_size => 24)
+                 :align => :left, :size => 24)
         
       end
       v_offset = 24 * target.users.length
     else
       pdf.text((target.profile.nil? ? 'no real name on file' :
                 target.profile.real_name),
-               :justification => :left, :font_size => 24)
+               :align => :left, :size => 24)
       v_offset = 24
     end
     
     # Submissions
     pdf.y = 792 - 36 - (36 + 24 + v_offset) - 40
-    pdf.select_font "Times-Roman"
-    PDF::SimpleTable.new do |table|
-      table.font_size = 12
-      table.heading_font_size = 14
-      table.orientation = :center
-      table.shade_rows = :striped
-      table.shade_color = Color::RGB.from_fraction(1, 1, 1)
-      table.shade_color2 = Color::RGB.from_fraction(0.95, 0.95, 0.95)
-      table.title = "Submissions for #{assignment.name}"
-      table.title_font_size = 24
-      
-      table.column_order.push('name', 'size', 'diagnostic', 'submitted',
-                              'extension')
-      [['name', 'Name', 170, :left], ['size', 'Size', 70, :right],
-       ['diagnostic', 'Diagnostic', 100, :left],
-       ['submitted', 'Submitted', 110, :center],
-       ['extension', 'Extension', 80, :center]
-      ].each do |cname, ctitle, cpts, cjust|
-        table.columns[cname] = PDF::SimpleTable::Column.new(cname) do |column|
-          column.heading = PDF::SimpleTable::Column::Heading.new do |heading|
-            heading.title = ctitle
-            heading.bold = true
-            heading.justification = :left
-          end
-          column.justification = cjust
-          column.width = cpts
-        end
-      end      
-
-      if assignment.deliverables.empty?
-        table.data = [{}]
+    pdf.font "Times-Roman"
+    table_data = assignment.deliverables.map do |d|
+      s = submissions[d.id]
+      if s.nil?
+        [d.name, 'N / A', 'no submission', 'N / A', 'N / A']
       else
-        table.data = assignment.deliverables.map do |d|
-          s = submissions[d.id]
-          if s.nil?
-            {'name' => d.name, 'size' => 'N / A',
-             'diagnostic' => 'no submission', 'time' => 'N / A',
-             'extension' => 'N / A'}
-          else
-            submitted_text = (s.updated_at < d.assignment.deadline) ?
-                'on time' : 'late by ' +
-                distance_of_time_in_words(d.assignment.deadline, s.updated_at)
-            if target.respond_to?(:users)
-              submitted_text += ' by ' +
-                  (user.profile.nil? ? user.name : user.profile.real_name)
-            end
-            {'name' => d.name, 
-             'size' => number_to_human_size(s.code.size),
-             'diagnostic' => (s.run_result.nil? ? 'no diagnostic' :
-                                                  s.run_result.diagnostic),
-             'submitted' => submitted_text,
-             'extension' => (s.updated_at < d.assignment.deadline) ? 'not needed' : ''}
-          end
+        submitted_text = (s.updated_at < d.assignment.deadline) ?
+            'on time' : 'late by ' +
+            distance_of_time_in_words(d.assignment.deadline, s.updated_at)
+        if target.respond_to?(:users)
+          submitted_text += ' by ' +
+              (user.profile.nil? ? user.name : user.profile.real_name)
         end
+        [d.name, 
+         number_to_human_size(s.code.size),
+         (s.run_result.nil? ? 'no diagnostic' : s.run_result.diagnostic),
+         submitted_text,
+         (s.updated_at < d.assignment.deadline) ? 'not needed' : '']
       end
-      
-      table.render_on pdf
     end
+
+    pdf.text "Submissions for #{assignment.name}", :size => 24, :align => :center
+    pdf.table table_data, :size => 12, :row_colors => ['f3f3f3', 'ffffff'],
+        :headers => ['Name', 'Size', 'Validation', 'Submitted', 'Extension'],
+        :align => { 0 => :left, 1 => :right, 2 => :left, 3 => :center,
+                    4 => :center},
+        :column_widths => {0 => 170, 1 => 70, 2 => 100, 3 => 110, 4 => 80},
+        :align_headers => :left, :position => :center do |t|
+      t.row(1).style :font_style => :bold, :background_color => 'ffffff'
+    end
+    pdf.y -= 2
     pdf.text "Please contact the course staff as soon as possible " +
              "if any of your submissions is not listed above.",
-             :justification => :center, :font_size => 13
+             :align => :center, :size => 13
 
     # Grades
-    pdf.move_pointer 40
-    pdf.select_font "Times-Roman"
-    PDF::SimpleTable.new do |table|
-      table.font_size = 12
-      table.heading_font_size = 14
-      table.orientation = :center
-      table.shade_rows = :striped
-      table.shade_color = Color::RGB.from_fraction(1, 1, 1)
-      table.shade_color2 = Color::RGB.from_fraction(0.95, 0.95, 0.95)
-      table.title = "Grades for #{assignment.name}"
-      table.title_font_size = 24
-      table.row_gap = 6
-      
-      table.column_order.push 'problem', 'grade', 'grader', 'points', 'comments'
-      [['problem', 'Problem', 140, :left], 
-       ['grade', 'Grade', 80, :right], 
-       ['grader', 'Grader', 70, :center],
-       ['points', 'Points', 70, :right], 
-       ['comments', 'Comments', 110, :center]].each do |cname, ctitle, cpts, cjust|
-        table.columns[cname] = PDF::SimpleTable::Column.new(cname) do |column|
-          column.heading = PDF::SimpleTable::Column::Heading.new do |heading|
-            heading.title = ctitle
-            heading.bold = true
-            heading.justification = :left
-          end
-          column.justification = cjust
-          column.width = cpts
-        end
-      end      
-
-      if assignment.assignment_metrics.empty?
-        table.data = [{}]
-      else
-        table.data = assignment.assignment_metrics.map do |m|        
-          {'problem' => m.name,
-          'points' => m.max_score.to_s,
-          'grader' => '',
-          'graded' => '', 'grade' => '', 'comments' => ''}
-        end      
-      end
-      table.render_on pdf
+    pdf.y -= 40
+    pdf.font "Times-Roman"
+    table_data = assignment.assignment_metrics.map do |m|        
+      [m.name,
+       '',
+       '',
+       m.max_score.to_s,
+       ''
+      ]
     end
-    pdf.text "Please check the course site if any of your grades is not listed above.", :justification => :center, :font_size => 13
+    pdf.text "Grades for #{assignment.name}", :size => 24, :align => :center
+    pdf.table table_data, :font_size => 12, :row_colors => ['f3f3f3', 'ffffff'],
+        :headers => ['Problem', 'Grade', 'Grader', 'Points', 'Comments'],
+        :align => { 0 => :left, 1 => :right, 2 => :center, 3 => :right,
+                    4 => :center},
+        :column_widths => { 0 => 140, 1 => 70, 2 => 70, 3 => 70, 4 => 110},
+        :align_headers => :left, :position => :center do |t|
+      t.row(0).style :font_style => :bold
+    end
+    pdf.y -= 2
+    pdf.text "Please check the course site if any of your grades is not " +
+             "listed above.", :align => :center, :size => 13
     
-    pdf.y = 40 + 24 * 3 + 60
+    # Total score
     pdf.font_size = 24
-    pdf.text "Total score: #{' ' * 10} out of #{assignment.assignment_metrics.inject(0) { |acc, m| acc + m.max_score }}"
-            
+    pdf.y = 792 - 36 - 24
+    max_score = assignment.assignment_metrics.map(&:max_score).sum
+    pdf.text "/ #{max_score}", :align => :right, :size => 36
+                
     if file_name.nil?
       pdf.to_s
     else
-      pdf.save_as file_name
+      pdf.render_file file_name
     end
   end
 end
