64c8aca04e905f40551fbbbf26eebcaa9fb01124
Victor Costan <costan@gmail.com>
2010-04-26 14:11:55 -0400

    Style tweaks.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
     size = options[:size] || :large
     size_pixels = {:small => 18, :large => 36}[size]
     image_tag user.gravatar_url(:size => size_pixels),
-              :alt => "gravatar for #{user.name}"
+              :alt => "gravatar for #{user.name}",
+              :style => "width: #{size_pixels}px; height: #{size_pixels}px;"
   end
 end
