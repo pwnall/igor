181333ce4568bbf7e19030dd408e3131c679b087
Victor Costan <costan@gmail.com>
2011-10-21 10:32:03 -0400

    Hurried config changes.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '>= 3.1.0'
+gem 'rails', '>= 3.1.1'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
@@ -12,11 +12,11 @@ gem 'json', '>= 1.5.4'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', "  ~> 3.1.0"
-  gem 'coffee-rails', "~> 3.1.0"
+  gem 'sass-rails', '>= 3.1.0'
+  gem 'coffee-rails', '>= 3.1.0'
   gem 'uglifier'
   
-  gem 'therubyracer'
+  gem 'therubyracer', '>= 0.9.8'
 end
 
 gem 'jquery-rails', '>= 1.0.13'
@@ -37,6 +37,7 @@ gem 'dynamic_form', '>= 1.1.4'
 gem 'fastercsv', '>= 1.5.4', :platforms => [:mri_18]
 gem 'gravtastic', '>= 3.2.6'
 gem 'mit_stalker', '>= 1.0.3'
+gem 'nokogiri', '>= 1.5.0'
 gem 'paperclip', :git => 'git://github.com/patshaughnessy/paperclip.git',
                  :ref => '942a2b43383fd04c0434ca4b27379a114cb28a50'
 gem 'prawn', '~> 0.11.1'
---
+++ script/load_stellar.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+unless ARGV[0]
+  puts "Usage: #{$0} [stellar_email_file]"
+  exit 1
+end
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
+
+doc = Nokogiri.HTML File.read(ARGV[0])
+users = Hash[doc.css('table tbody tr.recipient.participantRole').map do |tr|
+  cells = tr.css('td').map(&:inner_text)
+  name, email = *cells
+  name = $2 + ' ' + $1 if /(.+), (.+)/ =~ name
+  [name, email]
+end]
+
+course = Course.main
+
+users.each do |name, email|
+  user = User.create :email => email,  :password => email.reverse,
+                     :password_confirmation => email.reverse
+  user.active = true
+  user.save!
+
+  first_name = name.split(' ', 2).first
+  Profile.create! :user => user, :name => name,
+      :nickname => first_name, :university => 'MIT', :allows_publishing => true,
+      :department => 'Electrical Eng & Computer Sci', :year => 1.to_s,
+      :athena_username => email.split('@', 2).first,
+      :about_me => "Imported from Stellar"
+
+  registration = Registration.create! :user => user, :course => course,
+      :dropped => false, :for_credit => true, :motivation => "_"  
+end
