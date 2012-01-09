d46b2fd4343963fdbf1fdd7fd58ae7492a7e9737
Victor Costan <costan@gmail.com>
2012-01-09 00:23:18 -0500

    Integrated with revised authpwn reset password API.

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,14 +1,12 @@
 class SessionMailer < ActionMailer::Base
   include Authpwn::SessionMailer
 
-  # The subject line in a password reset e-mail.
-  def reset_password_subject(token, root_url)
-    "Password reset for #{Course.main.number} server at #{root_url}"
+  def reset_password_subject(token, server_hostname)
+    "#{Course.main.number} password reset"
   end
   
   # The sender e-mail address for a password reset e-mail.
   def reset_password_from(token, root_url)
-    # You must replace the e-mail address below.
     "#{Course.main.number} staff <#{Course.main.email}>"
   end
 
