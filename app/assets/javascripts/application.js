d40cdd4a514f6642db7e8fdcf70a52a950fccd28
pH14 <pwh@mit.edu>
2012-10-10 03:33:57 -0400

    adding best_in_place visual confirmations back in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,14 @@
 
 $(document).ready(function() {
   /* Activating Best In Place */
-  jQuery(".best_in_place").best_in_place();
+  jQuery(".best_in_place").best_in_place()
+                    .bind('ajax:success', function(e) {
+                        console.log(e);
+                        $(this).closest('tr').effect('highlight', {}, 2000);
+                     })
+                    .bind('best_in_place:error', function(e, error) {
+                        var errors = $.parseJSON(error.responseText);
+                        $('.validation-errors').html("Error: "+errors);
+                        $(this).closest('tr').effect('highlight', {color: 'red'}, 2000);
+                     });
 });
