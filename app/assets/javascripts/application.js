7dac3b8d2d4cb0ce4991383ce06d9d5c0bf69abd
pH14 <pwh@mit.edu>
2012-10-12 22:00:48 -0400

    factoring out recitation-page javascript code, trying to go by Rails 3's asset pipeline suggestions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,20 +8,7 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require pwn-fx
-//= require_tree .
 //= require jquery.purr
 //= require best_in_place
+//= require_tree .
 
-$(document).ready(function() {
-  /* Activating Best In Place */
-  jQuery(".best_in_place").best_in_place()
-                    .bind('ajax:success', function(e) {
-                        console.log(e);
-                        $(this).closest('tr').effect('highlight', {}, 2000);
-                     })
-                    .bind('best_in_place:error', function(e, error) {
-                        var errors = $.parseJSON(error.responseText);
-                        $('.validation-errors').html("Error: "+errors);
-                        $(this).closest('tr').effect('highlight', {color: 'red'}, 2000);
-                     });
-});
