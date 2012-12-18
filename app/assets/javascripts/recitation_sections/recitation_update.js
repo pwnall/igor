22a166f70585ce47894a63ce8b4073b0c4c3802e
Paul Hemberger <pH14@mit.edu>
2012-12-18 14:52:19 -0500

    cleaning up JS a little bit

--- app/assets/javascripts/recitation_sections/recitation_update.js
+++ app/assets/javascripts/recitation_sections/recitation_update.js
@@ -1,12 +1,12 @@
 /* Activating Best In Place */
 $(function() {
-    $(".best_in_place").best_in_place()
-        .bind('ajax:success', function(e) {
-            $(this).closest('tr').effect('highlight', {}, 2000);
-         })
-        .bind('best_in_place:error', function(e, error) {
-            var errors = $.parseJSON(error.responseText);
-            $('.validation-errors').html("Error: "+errors);
-            $(this).closest('tr').effect('highlight', {color: 'red'}, 2000);
+    $(".best_in_place").best_in_place().
+         bind('ajax:success', function() {
+             $(this).closest('tr').effect('highlight', {}, 2000);
+         }).
+         bind('best_in_place:error', function(e, error) {
+             var errors = $.parseJSON(error.responseText);
+             $('.validation-errors').html("Error: "+errors);
+             $(this).closest('tr').effect('highlight', {color: 'red'}, 2000);
          });
 });
--- app/assets/javascripts/registrations/registration_update.js
+++ app/assets/javascripts/registrations/registration_update.js
@@ -2,11 +2,11 @@
 $(function() {
     var updated_registration;
     
-    $("#registrations-table")
-        .bind('ajax:beforeSend', function(e, xhr, settings) {
-            update_registration = e.target;
-        })
-        .bind('ajax:success', function(xhr, data, status) {
-            $(update_registration).closest('tr').effect('highlight', {}, 3000);
-         });
+    $("#registrations-table").
+        bind('ajax:beforeSend', function(e) { 
+           updated_registration = e.target;
+    }).
+        bind('ajax:success', function() {
+           $(updated_registration).closest('tr').effect('highlight', {}, 3000);
+    });
 });
