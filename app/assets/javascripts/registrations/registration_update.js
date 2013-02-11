1ea76d22a89f2e9910d15fd6a1dc5c8c8e37e4c2
Paul Hemberger <pH14@mit.edu>
2013-02-10 21:31:42 -0500

    making registration ajax compatible with jQuery 1.9

--- app/assets/javascripts/registrations/registration_update.js
+++ app/assets/javascripts/registrations/registration_update.js
@@ -2,11 +2,11 @@
 $(function() {
     var updated_registration;
     
-    $("#registrations-table").
-        bind('ajax:beforeSend', function(e) { 
-           updated_registration = e.target;
-    }).
-        bind('ajax:success', function() {
-           $(updated_registration).closest('tr').effect('highlight', {}, 3000);
-    });
+    $(document)
+        .ajaxSend(function(e) {
+            updated_registration = e.target.activeElement.id;
+        })
+        .ajaxSuccess(function() {
+            $('#'+updated_registration).closest('tr').effect('highlight', {}, 3000);
+        });
 });
