ead2d4696f7018cdf643ad214e36687264965e15
Victor Costan <costan@gmail.com>
2013-09-18 19:10:12 -0400

    Fix crash when retrieving user submissions.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,11 +1,11 @@
 class SubmissionsController < ApplicationController
   include CoverSheet
-  
+
   before_filter :authenticated_as_user,
                 :only => [:new, :create, :update, :file, :info]
   before_filter :authenticated_as_admin,
                 :except => [:new, :create, :update, :file, :info]
-  
+
   # XHR /submissions/xhr_deliverables?assignment_id=1
   def xhr_deliverables
     @assignment = Assignment.where(:id => params[:assignment_id]).
@@ -22,17 +22,17 @@ class SubmissionsController < ApplicationController
     @assignments = Assignment.order('deadline DESC').includes(:deliverables).all
     @deliverables = @assignments.map(&:deliverables).flatten
   end
-  
+
   # GET /submissions
   def index
     @assignments = Assignment.order('deadline DESC').includes(:deliverables).
         all.reject { |assignment| assignment.deliverables.empty? }
     @deliverables = @assignments.map(&:deliverables).flatten
-    
+
     query = Submission.order('updated_at DESC').
         includes(:db_file, :analysis,
                  {:deliverable => :assignment}, :subject)
-    
+
     if params.has_key? :deliverable_id
       query = query.where(:deliverable_id => params[:deliverable_id])
     end
@@ -64,57 +64,57 @@ class SubmissionsController < ApplicationController
       format.html # new.html.erb
       format.js   # new.js.rjs
     end
-  end  
+  end
 
   # POST /submissions
-  def create    
+  def create
     deliverable = Deliverable.find params[:submission][:deliverable_id]
     return bounce_user unless deliverable.can_submit? current_user
-    
+
     @submission = deliverable.submission_for current_user
     @submission ||= Submission.new params[:submission]
     @submission.subject = current_user
-    
+
     success = if @submission.new_record?
       @submission.save
     else
       @submission.update_attributes(params[:submission])
     end
-    
+
     respond_to do |format|
       if success
         @submission.queue_analysis
-        
+
         flash[:notice] = "Uploaded #{@submission.db_file.f.original_filename} for #{@submission.assignment.name}: #{@submission.deliverable.name}."
         format.html { redirect_to @submission.assignment || root_path }
       else
         flash[:notice] = "Submission for #{@submission.assignment.name}: #{@submission.deliverable.name} failed."
         format.html { redirect_to @submission.assignment || root_path }
       end
-    end    
+    end
   end
-  
+
   # PUT /submissions/1
   def update
     create
   end
-  
+
   # POST /submissions/1/reanalyze
   def reanalyze
     @submission = Submission.find params[:id]
     @submission.queue_analysis
-    
+
     flash[:notice] = "Reanalysis request queued."
     respond_to do |format|
       format.html { redirect_to @submission.analysis }
-    end    
+    end
   end
-  
+
   # GET /submissions/1/file
   def file
     @submission = Submission.find(params[:id])
     return bounce_user unless @submission.can_read? current_user
-    
+
     db_file = @submission.full_db_file
     filename = @submission.subject.email.gsub(/[^A-Za-z0-9]/, '_') + '_' +
         db_file.f.original_filename
@@ -122,7 +122,7 @@ class SubmissionsController < ApplicationController
               :type => db_file.f.content_type,
               :disposition => params[:inline] ? 'inline' : 'attachment'
   end
-  
+
   # GET /submissions/1/info
   def info
     @submission = Submission.find(params[:id])
@@ -133,12 +133,12 @@ class SubmissionsController < ApplicationController
   # GET /submissions/package_assignment
   def package_assignment
     @assignment = Assignment.find(params[:assignment_id])
-    
+
     deliverable_ids = params[:package_include].select { |k, v| v == '1' }.
                                                map(&:first)
     @deliverables = Deliverable.where(:id => deliverable_ids).
                                 includes(:assignment)
-    
+
     if @deliverables.empty?
       # Admin only requested cover sheets, so find all users that submitted
       # anything for this assignment.
@@ -152,73 +152,60 @@ class SubmissionsController < ApplicationController
       seed_submissions = seed_submissions.where(
           Submission.arel_table[:updated_at].gteq(cutoff))
     end
-    
-    user_ids = seed_submissions.map(&:user_id).uniq
-    if team_partition = @assignment.team_partition
-      user_ids = User.where(:id => user_ids).map { |u|
-        team_partition.team_for_user(u) }.reject(&:nil?).
-                       map { |team| team.memberships.map(&:user_id) }.
-                       flatten.uniq
-    end
-    @users = User.where :id => user_ids
-    @user_submissions = Submission.where(:deliverable_id => deliverable_ids,
-                                         :user_id => user_ids).
-                                   includes(:deliverable, :user).
-                                   order(:id)
-    
-    if @user_submissions.empty?
+
+    @subjects = seed_submissions.map(&:subject).uniq
+    subject_ids = @subjects.map(&:id)
+    @submissions = Submission.where(deliverable_id: deliverable_ids,
+                                    subject_id: subject_ids).
+                              includes(:deliverable, :subject).order(:id)
+
+    if @submissions.empty?
       flash[:alert] = 'There is no submission meeting your conditions.'
       request_package
       render :action => :request_package
       return
     end
-    
-    if team_partition
-      @teams = @users.map { |u| team_partition.team_for_user(u) }.uniq
-    else
-      @teams = nil
-    end
-    
+
     temp_dir = with_temp_dir do |tempdir|
       # write out submissions
       unless @deliverables.empty?
-        @user_submissions.each do |s|
+        @submissions.each do |s|
           prefix = params[:filename_prefix][s.deliverable.id.to_s] || ''
           suffix = params[:filename_suffix][s.deliverable.id.to_s] || ''
-          basename = if team_partition
-            team_partition.team_for_user(s.user).name.underscore.gsub(' ', '_')
-          else
-            s.user.email.split('@').first
-          end
+          subject = s.subject
+          basename = subject.respond_to?(:email) ?
+              subject.email.split('@').first :
+              subject.name.underscore.gsub(' ', '_')
           db_file = s.full_db_file
           extension = db_file.f.original_filename.split('.').last
           fname = "#{tempdir}/#{prefix}#{basename}#{suffix}.#{extension}"
           File.open(fname, 'wb') { |f| f.write db_file.f.file_contents }
         end
       end
-      
+
       # generate cover sheets
       if params[:package_include]['cover'] == '1'
-        (@teams || @users).each do |target|
+        @subjects.each do |subject|
           prefix = params[:filename_prefix]['cover'] || ''
           suffix = params[:filename_suffix]['cover'] || ''
-          basename = team_partition ? target.name.underscore.gsub(' ', '_') :
-                                      target.email.split('@').first          
+          basename = subject.respond_to?(:email) ?
+              subject.email.split('@').first :
+              subject.name.underscore.gsub(' ', '_')
           extension = 'pdf'
           fname = "#{tempdir}/#{prefix}#{basename}#{suffix}.#{extension}"
-          cover_sheet_for_assignment target, @assignment, fname
-        end        
+          cover_sheet_for_assignment subject, @assignment, fname
+        end
       end
-      
+
       # zip up the submissions
       cur_dir = Dir.pwd
       Dir.chdir(tempdir) { Kernel.system "zip #{cur_dir}/#{tempdir}.zip *" }
       package_fname = "assignment_#{@assignment.id}.zip"
-      send_data File.open("#{tempdir}.zip", 'rb') { |f| f.read }, :filename => package_fname, :disposition => 'inline'      
+      send_data File.open("#{tempdir}.zip", 'rb') { |f| f.read }, :filename => package_fname, :disposition => 'inline'
     end
-    File.delete "#{temp_dir}.zip"    
+    File.delete "#{temp_dir}.zip"
   end
-  
+
   def with_temp_dir
     temp_dir = 'tmp/' + (0...16).map { rand(256).to_s(16) }.join
     Dir.mkdir temp_dir
--- lib/user_filters.rb
+++ lib/user_filters.rb
@@ -1,11 +1,8 @@
 module UserFilters
   # (before-filter) ensures that the session belongs to a registered user
   def authenticated_as_user
-    puts 'inside user auth'
     return bounce_user if current_user.nil?
-    puts 'past bounce (cu.nil == false)'
     return true if current_user.email_credential.verified?
-    puts 'past email ver. bad.'
     # Inactive user.
     set_session_current_user nil
     bounce_user
