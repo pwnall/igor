88174c2a7faf997e1ddac085b5afbfaca9940eac
Ong Ming Yang <mingy@mit.edu>
2014-04-02 19:19:32 -0400

    Code refactoring

--- test/controllers/grades_controller_test.rb
+++ test/controllers/grades_controller_test.rb
@@ -5,18 +5,17 @@ describe GradesController do
 
   describe 'authenticated as a normal user' do
     before do
-      @user = users(:dexter)
+      @user = users :dexter
       set_session_current_user users(:dexter)
     end
 
     it 'user can view /grades page' do
-    # it tests the before_filter in the grades controller
       get :index
       assert_response :success
     end
 
     it 'user cannot view other pages' do
-    # it tests the before_filter in the grades controller
+      # 403 refers to a forbidden page
       get :editor
       assert_response 403
       post :create
@@ -32,44 +31,47 @@ describe GradesController do
 
   describe 'authenticated as a grader' do
     before do
-      @grader = users(:admin)
+      @grader = users :admin
       set_session_current_user users(:admin)
-      @student = users(:dexter)
-      @problem = assignment_metrics(:assessment_overall)
+      @student = users :dexter
+      @problem = assignment_metrics :assessment_overall
       @params = { grade: {
-              subject_id: @student.exuid, 
-              subject_type: 'User',
-              metric_id: @problem.id, 
-              score: 4.0 }, 
-              comment: { comment: '' }
-              }
+          subject_id: @student.to_param, 
+          subject_type: 'User',
+          metric_id: @problem.to_param, 
+          score: 4.0 }, 
+          comment: { comment: '' }
+          }
     end
 
     it 'can update existing grades without a comment' do
-      scoresarray = [1.0, 1, 0.0, 0]
-      assert_no_difference('GradeComment.count') do
-        scoresarray.each do |score|
-          @params[:grade][:score] = score
-          post :create, 
-              grade: @params[:grade], 
-              comment: @params[:comment]
-          assert_equal score, Grade.where(
-              subject_type: 'User',
-              subject_id: @student.id,
-              metric_id: @problem.id).first.score
-        end
+      scores = [1.0, 1, 0.0, 0]
+      scores.each do |score|
+        @params[:grade][:score] = score
+        post :create, 
+            grade: @params[:grade], 
+            comment: @params[:comment]
+        grade = Grade.where(
+            subject_type: 'User',
+            subject_id: @student.id,
+            metric_id: @problem.id)
+        assert_equal score, grade.first.score
+        assert_equal nil, grade.first.comment
       end
     end
 
     it 'can create new grades without a comment' do
-      # change problem to one that has not been already graded
+      # Change problem to one that has not been already graded
       @params[:grade][:metric_id] = assignment_metrics(:assessment_quality).id
-      assert_no_difference('GradeComment.count') do
-        assert_difference('Grade.count') do
-          post :create, 
-              grade: @params[:grade], 
-              comment: @params[:comment]
-        end
+      assert_difference 'Grade.count' do
+        post :create, 
+            grade: @params[:grade], 
+            comment: @params[:comment]
+        assert_response :success
+        assert_equal nil, Grade.where(
+            subject_type: 'User',
+            subject_id: @student.id,
+            metric_id: @problem.id).first.comment
       end
     end
 
@@ -77,14 +79,20 @@ describe GradesController do
       problems = [@problem, assignment_metrics(:assessment_quality)]
       problems.each do |problem|
         @params[:grade][:metric_id] = problem.id
-        assert_difference('GradeComment.count') do
+        assert_difference 'GradeComment.count' do
           post :create, 
               grade: @params[:grade], 
               comment: { comment: 'New comment!' }
+          assert_response :success
+          assert_equal 'New comment!', Grade.where(
+              subject_type: 'User',
+              subject_id: @student.id,
+              metric_id: problem.id).first.comment.comment
         end
         post :create, 
             grade: @params[:grade], 
             comment: { comment: 'Changed comment!' }
+        assert_response :success
         assert_equal 'Changed comment!', Grade.where(
             subject_type: 'User',
             subject_id: @student.id,
@@ -92,5 +100,4 @@ describe GradesController do
       end
     end
   end
-
 end
