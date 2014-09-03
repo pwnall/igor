ea008bc16b0a977bbd0bbefa0a5d5fd19151c94b
Victor Costan <costan@gmail.com>
2014-09-03 02:11:38 -0400

    Tried to make staff registration link more visible.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -2,6 +2,20 @@ unless Rails.env.test?
   Rails.application.config.action_mailer.delivery_method = :smtp
 end
 
-Rails.application.config.action_mailer.smtp_settings = {
-  address: 'outgoing.mit.edu', port: 25, domain: 'mit.edu',
-}
+if Rails.env.production?
+  Rails.application.config.action_mailer.smtp_settings = {
+    address: 'outgoing.mit.edu',
+    port: 25,
+    domain: 'mit.edu',
+  }
+else  # Development and test.
+  Rails.application.config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: Rails.application.secrets.gmail_email.split('@').last,
+    user_name: Rails.application.secrets.gmail_email,
+    password: Rails.application.secrets.gmail_password,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+end
