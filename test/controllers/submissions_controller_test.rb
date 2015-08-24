08e32cb6b26ceba54bc7556aec62ec1977bc0a94
Staphany Park <stapark008@gmail.com>
2015-08-24 11:55:41 -0400

    More SubmissionsController tests.

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -14,20 +14,54 @@ class SubmissionsControllerTest < ActionController::TestCase
   end
 
   describe 'POST #create' do
-    it 'reuses the collaborators from the previous submission' do
-      previous_submission = Submission.where(subject: users(:dexter),
-          deliverable: @deliverable).last
-
-      assert_difference 'Submission.count' do
-        post :create, params: { course_id: @params[:course_id],
-            submission: @params[:submission] }
+    describe 'no submissions from the current user for the deliverable' do
+      before do
+        @deliverable.submissions.where(subject: users(:dexter)).destroy_all
       end
 
-      latest_collaborators = Submission.last.collaborators
+      it 'creates a submission with no collaborators' do
+        assert_difference 'Submission.count' do
+          post :create, params: { course_id: @params[:course_id],
+              submission: @params[:submission] }
+        end
 
-      assert_equal [users(:deedee)].to_set, latest_collaborators.to_set
-      assert_equal latest_collaborators.to_set,
-          previous_submission.collaborators.to_set
+        assert_empty Submission.last.collaborators
+      end
+
+      it "redirects to the deliverable's assignment page" do
+        post :create, params: { course_id: @params[:course_id],
+            submission: @params[:submission] }
+
+        assert_redirected_to assignment_url(@deliverable.assignment,
+            course_id: @deliverable.course)
+      end
+    end
+
+    describe 'current user has previously submitted for the deliverable' do
+      it 'reuses the collaborators from the previous submission' do
+        previous_submission = Submission.where(subject: users(:dexter),
+            deliverable: @deliverable).last
+        assert_not_nil previous_submission
+
+        assert_difference 'Submission.count' do
+          post :create, params: { course_id: @params[:course_id],
+              submission: @params[:submission] }
+        end
+
+        latest_collaborators = Submission.last.collaborators
+
+        assert_equal [users(:deedee)].to_set, latest_collaborators.to_set
+        assert_equal latest_collaborators.to_set,
+            previous_submission.collaborators.to_set
+      end
+
+      it "redirects to the deliverable's assignment page" do
+        post :create, params: { course_id: @params[:course_id],
+            submission: @params[:submission] }
+
+        assert_redirected_to assignment_url(@deliverable.assignment,
+            course_id: @deliverable.course)
+      end
     end
   end
 end
