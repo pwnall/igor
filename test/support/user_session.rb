d9f7745949fe27a06f4021dd477c0f788d952cc0
Staphany Park <stapark008@gmail.com>
2015-09-25 17:39:48 -0400

    Integration test for uploading submissions.

--- test/support/user_session.rb
+++ test/support/user_session.rb
@@ -1,3 +1,12 @@
 class ActionDispatch::IntegrationTest
-  
+  # Log in as the given user.
+  def log_in_as(user)
+    password = (user == users(:dexter)) ? 'pa55w0rd' : 'password'
+
+    visit '/'
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: password
+    click_button 'Log in'
+    assert_equal '/6.006', current_path
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/mini_test'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -9,6 +12,39 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+
+  before { Capybara.current_driver = Capybara.javascript_driver }
+
+  # Check every second for the given CSS selector, before executing the block.
+  #
+  # Useful for XHR requests that are purposely delayed.
+  def wait_for_selector(selector)
+    5.times do
+      break if page.has_css? selector
+      sleep 1
+    end
+    yield
+  end
+end
+
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+# Monkey-patch to share database transactions across threads.
+#
+# Without shared transactions, Capybara will not see the data in the database.
+# Also, Delayed::Worker breaks in integration tests without this patch, perhaps
+# due to a threading problem.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
 # The 'test/support/' directory contains helper methods for writing tests.
 Dir[Rails.root.join('test', 'support', '*.rb')].each { |file| require file }
