4a98e5963fb9d3db2984e05b8142a43ab3f5a94f
Victor Costan <costan@gmail.com>
2015-11-26 01:52:17 -0500

    Workaround fontawesome / rails5 bug.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,11 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+// TODO(pwnall): Remove this workaround when font-awesome-rails gets Rails 5
+//               support.
+//= link fontawesome-webfont.eot
+//= link fontawesome-webfont.woff2
+//= link fontawesome-webfont.woff
+//= link fontawesome-webfont.ttf
+//= link fontawesome-webfont.svg
